1723ffe99c675361d13d3a1f9cd35f38c9e0a26e
Paguiel <119151331+paguielng@users.noreply.github.com>
2026-01-24 01:09:31 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+export default defineConfig({
+  plugins: [react()],
+  base: '/'
+})
+```
+
+### 4. **.gitignore**
+```
+node_modules
+dist
+.DS_Store
+*.log
